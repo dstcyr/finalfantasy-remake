996b8406a0e0d63ac8a32073518e132a2fe44fc0
David St-Cyr <st-cyrdavid@hotmail.com>
2025-05-22 10:55:10 -0400

    Testing ui image in menu

--- FinalFantasy/Menu.cpp
+++ FinalFantasy/Menu.cpp
@@ -1,10 +1,15 @@
 #include "Menu.h"
 #include "Engine.h"
 #include "Log.h"
+#include "UI/UIImage.h"
+
+std::shared_ptr<UIImage> test;
 
 void Menu::OnEnter()
 {
     LOG(LL_DEBUG, "Menu::OnEnter");
+
+    test = UIImage::Create("Assets/Images/life.png");
 
 #if MENU_MOCKUP
     m_menuMockup = Engine::LoadTexture("Assets/Design/menu.png");
@@ -20,6 +25,8 @@ void Menu::OnRender()
 #if MENU_MOCKUP
     Engine::DrawTexture(m_menuMockup, 0.0f, 0.0f, NColor(255, 255, 255, 50));
 #endif
+
+    test->Draw(100.0F, 100.0F);
 }
 
 void Menu::OnExit()
